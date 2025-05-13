bfb7c05e8f5372a3ed301f607e7f99ef99773d1c
René Bischof <rene.bischoff@gmail.com>
2025-05-13 15:47:31 +0200

    fix: add blank test

---
+++ test/blank.test.ts
@@ -0,0 +1,8 @@
+import { assertEquals } from "jsr:@std/assert"
+import { describe, it } from "jsr:@std/testing/bdd"
+
+describe("BlankTest", () => {
+  it("should be true", () => {
+    assertEquals(true, true)
+  })
+})
