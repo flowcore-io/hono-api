2cc070f5ab0770273038bf1089c32b5dda127675
René Bischof <rene.bischoff@gmail.com>
2025-05-14 12:37:55 +0200

    fix: add environment class

---
+++ src/lib/environment.ts
@@ -0,0 +1,40 @@
+import type { z } from "zod"
+import process from "node:process"
+
+export enum NodeEnv {
+  Development = "development",
+  Production = "production",
+  Staging = "staging",
+  Test = "test",
+}
+
+export class Environment<T extends z.ZodObject<z.ZodRawShape>> {
+  private schema: T
+  private _env: z.infer<typeof this.schema> | undefined
+
+  constructor(schema: T) {
+    this.schema = schema
+  }
+
+  get env(): z.infer<T> {
+    if (this._env) {
+      return this._env
+    }
+
+    if (process.env.SKIP_ENV_VALIDATION) {
+      console.debug("Skipping env validation")
+      this._env = {} as z.infer<typeof this.schema>
+      return this._env
+    }
+
+    const parsed = this.schema.safeParse(process.env)
+
+    if (!parsed.success) {
+      console.error("Missing or invalid environment variables", { errors: parsed.error })
+      process.exit(1)
+    }
+
+    this._env = parsed.data
+    return this._env as z.infer<T>
+  }
+}
--- src/mod.ts
+++ src/mod.ts
@@ -1,2 +1,3 @@
 export * from "./lib/hono-api.ts"
 export * from "./lib/hono-api-router.ts"
+export * from "./lib/environment.ts"
